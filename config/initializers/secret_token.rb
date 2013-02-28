11725a1ee70721b67e7eb601e21c127646814324
Matt Scott <scoma@musc.edu>
2013-02-27 19:17:13 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flitter::Application.config.secret_token = '4177404fffe347b00c0bc4c9230d6e594879a9641bcb6f5d7243c10038534e91a62207705ecbd8fcd316a46f5b28e6b5b239c51633c690889d80260aa9d6d628'
