c268091aec33a3e55b329a4a66b05dd4baf237fd
Matt Scott <scoma@musc.edu>
2013-03-06 20:10:05 -0500

    Created User Actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      render :action => :show
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      render :action => :show
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      redirect_to users_path
+    else
+      render :show
+    end
+  end
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   attr_accessible :last_name
   attr_accessible :username
   attr_accessible :email
+  
+  def full_name
+    self.first_name + ' ' + self.last_name
+  end
+  
 end
